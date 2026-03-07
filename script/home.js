42585a1856bae773168dfede2cdef84606664199
MaksumulEmon <maksumulemon300@gmail.com>
2026-03-07 19:12:35 +0600

    Loading Spainer

--- script/home.js
+++ script/home.js
@@ -1,9 +1,26 @@
 
 const dataContainer = document.getElementById('dataContainer');
+const loadingSpiner =document.getElementById('loadingSpiner');
+
+
+
+function showLoading (){
+    loadingSpiner.classList.remove("hidden");
+    dataContainer.innerHTML ="";
+}
+
+function hiddenLoading(){
+       loadingSpiner.classList.add("hidden");
+}
+
+
 
 async function loadData() {
+    showLoading();
     const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
     const info = await res.json();
+ 
+    hiddenLoading();
     // console.log(data);
     displayData(info.data)
 
@@ -22,16 +39,22 @@ function displayData(datas) {
 
 
 
-
-
-        const card = document.createElement("div")
+ const card = document.createElement("div")
         card.className = `p-4 rounded-xl shadow-lg bg-white border-t-4 ${data.status === "open" ? 'border-t-[#00A96E]' : 'border-t-[#A855F7]'}`;
         card.innerHTML = `
         
                     <div class="flex justify-between items-center">
                    <img src="${data.status === "open" ? './assets/Open-Status.png' : './assets/Closed-Status.png'}" alt="" class="w-6 h-6" />
 
-                        <p class="text-[#ef4444] bg-[#FEECEC] px-6 py-1 rounded-4xl">${data.priority}</p>
+
+                <p class="${data.priority === "high"
+                ? "text-[#EF4444] bg-[#FEECEC] px-6 py-1 rounded-4xl"
+                : data.priority === "low"
+                    ? "text-[#9CA3AF] bg-[#EEEFF2] px-6 py-1 rounded-4xl"
+                    : "text-[#F59E0B] bg-[#FFF6D1] px-6 py-1 rounded-4xl"
+                    }">
+                     ${data.priority}
+                    </p>
                     </div>
 
                     <p class="text-[#1F2937] font-semibold pt-3 line-clamp-1">${data.title}</p>
@@ -42,15 +65,14 @@ function displayData(datas) {
                                 class="fa-solid fa-bug"></i> ${data.labels[0]}
                         <p>
                        <p class="text-[#D97706] bg-[#FFF8DB] px-2 py-1.5 border border-[#FDE68A] rounded-4xl ${data.labels[1] ? "" : "hidden"}">
-    <i class="fa-solid fa-life-ring"></i> ${data.labels[1] ? data.labels[1] : ""}
-</p>
-
+                          <i class="fa-solid fa-life-ring"></i> ${data.labels[1] ? data.labels[1] : ""}
+                        </p>
 
                     </div>
 
                     <div class="border-t border-zinc-400 mt-4 pt-4 pl-4 pr-4">
                         <p class="text-[#64748B]">${data.author}</p>
-                        <p class="text-[#64748B]">${data.updatedAt}</p>
+                        <p class="text-[#64748B]">${data.createdAt}</p>
 
                     </div>
         `;
